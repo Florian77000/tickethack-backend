23e012a3253c5370d7ea990f96c3c50fdeb1fef5
Florian <floriandelaunay75@gmail.com>
2025-04-15 14:25:00 +0200

    ajout des messages de routes trips/search

--- routes/trips.js
+++ routes/trips.js
@@ -1,9 +1,32 @@
 var express = require("express");
 var router = express.Router();
+const Trip = require("../models/trips");
 
-/* GET users listing. */
-router.get("/", function (req, res, next) {
-  res.json({ message: "routes trips ok" });
+router.get("/", (req, res) => {
+  Trip.find().then((data) => {
+    res.json({ data });
+  });
+});
+
+router.post("/search", (req, res) => {
+  if (!req.body.departure || !req.body.arrival || !req.body.date) {
+    return res.json({
+      result: true,
+      message: "merci de remplir tous les champs",
+    });
+  }
+  Trip.findOne({
+    departure: req.body.departure,
+    arrival: req.body.arrival,
+    date: req.body.date,
+    price: req.body.price,
+  }).then((data) => {
+    if (data) {
+      return res.json({ result: true, message: "voyages trouvés" });
+    } else {
+      return res.json({ result: false, message: "aucun voyages trouvés" });
+    }
+  });
 });
 
 module.exports = router;
