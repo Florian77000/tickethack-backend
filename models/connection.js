6821785c7844a38cfdd389b265991a58bea2e631
Florian <floriandelaunay75@gmail.com>
2025-04-15 14:29:00 +0200

    ajout de dotenv, env, et ligne 1 sur app.js

--- models/connection.js
+++ models/connection.js
@@ -1,8 +1,9 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const connectionString = 'mongodb+srv://marionrdx:X2JzBkzzAPzQ1q1Y@clustermarionradix.o5wu8g3.mongodb.net/tickethack';
+const connectionString = process.env.CONNECTION_STRING;
 
-mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
- .then(() => console.log('Database connected'))
+mongoose
+  .connect(connectionString, { connectTimeoutMS: 2000 })
+  .then(() => console.log("Database connected"))
 
-  .catch(error => console.error(error));
+  .catch((error) => console.error(error));
