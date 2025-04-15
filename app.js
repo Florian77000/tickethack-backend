de07b4abee1f151b94fdb5189b608472975d5a54
Florian <floriandelaunay75@gmail.com>
2025-04-15 12:06:50 +0200

    reajout des routes trips, bookings, carts

--- app.js
+++ app.js
@@ -1,23 +1,29 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var cartsRouter = require("./routes/carts");
+var bookingsRouter = require("./routes/bookings");
+var tripsRouter = require("./routes/trips");
 
 var app = express();
 
-const cors = require('cors');
+const cors = require("cors");
 app.use(cors());
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/carts", cartsRouter);
+app.use("/bookings", bookingsRouter);
+app.use("/trips", tripsRouter);
 
 module.exports = app;
